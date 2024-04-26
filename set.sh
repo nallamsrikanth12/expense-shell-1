1194fa046de966eb68ab4cd06ed9f0d1e4899bf6
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-26 23:39:22 +0630

    installation

--- set.sh
+++ set.sh
@@ -3,7 +3,7 @@ USERID=$(id -u)
 set -e
 
 handle_error(){
-    echo "error occure at line $1,error command $2"
+    echo "error occure at line $1, error command $2"
 }
 
 trap 'handle_error ${LINENO} "${BASH}_COMMAND"' ERR
