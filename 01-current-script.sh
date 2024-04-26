a58f4ab3cf3c40f1d5d01e874d05fa06a8deb76f
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-26 19:02:25 +0630

    installation

--- 01-current-script.sh
+++ 01-current-script.sh
@@ -3,7 +3,7 @@ COURSE="devops from current script"
 echo "before calling other script: $COURSE"
 echo "process id of current shell script: $$"
 
-./16-other-script.sh
+./02-other-script.sh
 
 echo "after calling other script,course:$COURSE"
 
