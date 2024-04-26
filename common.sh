af95d812039a6d5e49e06a8a6d4b870fe8d45ecb
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-27 01:59:40 +0630

    installation

--- common.sh
+++ common.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
+failure(){
+    echo "Failed at error occured $1, error: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -8,8 +16,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-echo "Please enter DB password:"
-read -s mysql_root_password
+
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -30,4 +37,5 @@ else
     echo "You are super user."
 fi
 }
+
 
--- db-sql.sh
+++ db-sql.sh
@@ -10,7 +10,7 @@ read -s mysql_root_password
 
 
 
-dnf install mysql-server -y &>>$LOGFILE
+dnf install mysqll-server -y &>>$LOGFILE
 #VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
